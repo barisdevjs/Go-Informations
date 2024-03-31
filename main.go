683026f74e39f4585f956f01cfba5dc65d079504
Barış SAVAŞ <barissavas17@gmail.com>
2024-04-01 01:46:22 +0300

    main.go file added for result printing

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	waitgroups "github.com/barisdevjs/Go-Informations/waitGroups"
+)
+
+func main() {
+
+	// generics.Generics()
+	// errors.Errors1()
+	// errors.PrintCustomError()
+	// goroutines.Goroutines1()
+	// channels.Channel1()
+	// channels.Channels4()
+	// channels.NonBlocking()
+	// channels.NonBlocking2()
+	// selectP.Select1()
+	// selectP.Select2()
+	// timeouts.Timeouts1()
+	// channels.CloseChannel()
+	// channels.RangeOverChannels1()
+	// channels.RangeOverChannels2()
+	// timer.Timer1()
+	// timer.Ticker1()
+	// channels.WorkerPool()
+	waitgroups.WaitGroup1()
+}
